eaebdc8c7aca3b096ddd313f5c1fca9fd790dba5
abramovich-maks <maks.abramovich.1995@mail.ru>
2025-10-26 11:30:29 +0100

    base integration tests and integration package

---
+++ src/integration/java/com/lotto/BaseIntegrationTest.java
@@ -0,0 +1,9 @@
+package com.lotto;
+
+import com.lotto.domain.LottoSpringBootApplication;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = LottoSpringBootApplication.class)
+public class BaseIntegrationTest {
+
+}
