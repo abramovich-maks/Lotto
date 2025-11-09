a0882791dea54662295d5d59c5b7ecabb7801551
abramovich-maks <maks.abramovich.1995@mail.ru>
2025-11-09 16:50:23 +0100

    add conflict error handling for duplicate user email

--- src/main/java/com/lotto/domain/loginandregister/UserAlreadyExistException.java
+++ src/main/java/com/lotto/domain/loginandregister/UserAlreadyExistException.java
@@ -2,10 +2,10 @@ package com.lotto.domain.loginandregister;
 
 public class UserAlreadyExistException extends RuntimeException {
 
-    private final String userEmail;
+    public final String userEmail;
 
     public UserAlreadyExistException(String userEmail) {
-        super(String.format("User with email %s already exists",userEmail));
+        super(String.format("eMail [%s] already exists", userEmail));
         this.userEmail = userEmail;
     }
 }
--- src/main/java/com/lotto/infrastructure/loginandregister/controller/error/TokenControllerErrorHandler.java
+++ src/main/java/com/lotto/infrastructure/loginandregister/controller/error/TokenControllerErrorHandler.java
@@ -1,5 +1,7 @@
 package com.lotto.infrastructure.loginandregister.controller.error;
 
+import com.lotto.domain.loginandregister.UserAlreadyExistException;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -7,7 +9,10 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+import java.util.Collections;
+
 @ControllerAdvice
+@Log4j2
 class TokenControllerErrorHandler {
 
     private static final String BAD_CREDENTIALS = "Bad credentials";
@@ -17,5 +22,14 @@ class TokenControllerErrorHandler {
     @ExceptionHandler(BadCredentialsException.class)
     public LoginErrorResponse handleBadCredentials() {
         return new LoginErrorResponse(BAD_CREDENTIALS, HttpStatus.UNAUTHORIZED);
+    }
+
+    @ResponseStatus(HttpStatus.CONFLICT)
+    @ExceptionHandler(UserAlreadyExistException.class)
+    @ResponseBody
+    public LoginErrorResponse mailDuplicate(UserAlreadyExistException duplicateMailException) {
+        final String message = String.format("eMail [%s] already exists", duplicateMailException.userEmail);
+        log.error(message);
+        return new LoginErrorResponse(message, HttpStatus.CONFLICT);
     }
 }
