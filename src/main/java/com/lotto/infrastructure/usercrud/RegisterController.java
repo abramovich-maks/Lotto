e1b78d09e0c8ba65f0eef7210f479cfe318c475e
abramovich-maks <maks.abramovich.1995@mail.ru>
2025-12-04 22:24:52 +0100

    secure passwords with bcrypt encoding in RegisterController

--- src/main/java/com/lotto/infrastructure/usercrud/RegisterController.java
+++ src/main/java/com/lotto/infrastructure/usercrud/RegisterController.java
@@ -1,9 +1,11 @@
 package com.lotto.infrastructure.usercrud;
 
 import lombok.AllArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,20 +15,23 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @AllArgsConstructor
 @RequestMapping()
+@Log4j2
 class RegisterController {
 
     private final UserDetailsManager userDetailsManager;
+    private final PasswordEncoder bCryptpasswordEncoder;
+
 
     @PostMapping("/register")
     public ResponseEntity<RegisterUserResponseDto> register(@RequestBody RegisterUserRequestDto dto) {
-        String password = dto.password();
+    String encodedPassword = bCryptpasswordEncoder.encode(dto.password());
         String username = dto.email();
         UserDetails user = User.builder()
                 .username(username)
-                .password(password)
+                .password(encodedPassword)
                 .authorities("ROLE_USER")
                 .build();
         userDetailsManager.createUser(user);
-        return ResponseEntity.ok(new RegisterUserResponseDto("Created user"));
+        return ResponseEntity.ok(new RegisterUserResponseDto("Success. User created."));
     }
 }
